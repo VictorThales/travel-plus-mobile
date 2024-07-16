fe483a54442acd8dd88541b2bed865e62300939c
Victor Thales <victordelimas@gmail.com>
2024-07-15 23:41:21 -0300

    feat: rename

--- src/routes/TabNavigator.tsx
+++ src/routes/TabNavigator.tsx
@@ -17,7 +17,7 @@ export const TabNavigator = () => {
   const user = useAuthStore((state) => state.user);
 
   const headerStyle: BottomTabNavigationOptions = {
-    headerTitle: 'Trip +',
+    headerTitle: 'Travel +',
     headerTitleStyle: { fontSize: 25, fontWeight: 'bold' },
     headerTitleAlign: 'center',
     headerTintColor: '#556B2F',
